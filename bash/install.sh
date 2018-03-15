dd5608cd3eb88ec7e16f819f505957f1dbc983a4
rux616 <5643061+rux616@users.noreply.github.com>
2018-03-15 11:33:17 -0600

    Modified to use common functions and to use quotes

--- bash/install.sh
+++ bash/install.sh
@@ -3,19 +3,16 @@
 # Get directory where this script lives, because that's where the bash files will also be
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Get common functions
+source "$DIR/../.common_functions.sh"
+
 # Back up existing user bash files
-if [ -w ~/.bash_profile ]; then
-    mv ~/.bash_profile ~/.bash_profile_old
-fi
-if [ -w ~/.bashrc ]; then
-    mv ~/.bashrc ~/.bashrc_old
-fi
-if [ -w ~/.bash_aliases ]; then
-    mv ~.bash_aliases ~/.bash_aliases_old
-fi
+back_up_file "~/.bash_profile"
+back_up_file " ~/.bashrc"
+back_up_file " ~/.bash_aliases"
 
 # Symlink bash files into their appropriate places
-ln -s $DIR/.bash_profile ~/.bash_profile
-ln -s $DIR/.bashrc ~/.bashrc
-ln -s $DIR/.bash_aliases ~/.bash_aliases
+ln -s "$DIR/.bash_profile" "~/.bash_profile"
+ln -s "$DIR/.bashrc" "~/.bashrc"
+ln -s "$DIR/.bash_aliases" "~/.bash_aliases"
 
--- vim/install.sh
+++ vim/install.sh
@@ -3,25 +3,24 @@
 # Get directory where this script lives, because that's where the files will also be
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Get common functions
+source "$DIR/../.common_functions.sh"
+
 # Back up existing files
-if [ -w ~/.vimrc ]; then
-    mv ~/.vimrc ~/.vimrc_old
-fi
-if [ -w ~/.vim/colors/badwolf ]; then
-    mv ~/.vim/colors/badwolf ~/.vim/colors/badwolf_old
-fi
+back_up_file "~/.vimrc"
+back_up_file "~/.vim/colors/badwolf.vim"
 
 # Attempt to create .vim/colors/
-mkdir -p ~/.vim/colors 2>/dev/null
+mkdir -p "~/.vim/colors" 2>/dev/null
 
 # Update or init badwolf submodule
-if [ -n $(ls $DIR/badwolf/) ]; then
+if [ -n $(ls "$DIR/badwolf/") ]; then
     git submodule update --remote
 else
     git submodule update --init
 fi
 
 # Symlink files into their appropriate places
-ln -s $DIR/.vimrc ~/.vimrc
-ln -s $DIR/badwolf/colors/badwolf.vim ~/.vim/colors/badwolf.vim
+ln -s "$DIR/.vimrc" "~/.vimrc"
+ln -s "$DIR/badwolf/colors/badwolf.vim" "~/.vim/colors/badwolf.vim"
 
