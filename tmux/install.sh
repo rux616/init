1a89e294dd8108747ec30af630a5aeb4704c2a5e
rux616 <5643061+rux616@users.noreply.github.com>
2019-02-06 14:54:03 -0700

    Added tmux config

---
+++ tmux/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Get directory where this script lives, because that's where the files will also be
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Get common functions
+source "$DIR/../.common_functions.sh"
+
+# Update or init submodules
+cd $DIR
+git submodule update --init -- .
+git submodule update --remote -- .
+
+# Back up existing files
+back_up_file "$HOME/.tmux.conf"
+
+# Attempt to create .tmux/plugins/
+mkdir -p "$HOME/.tmux/plugins" 2>/dev/null
+
+# Symlink files into their appropriate places
+ln -s "$DIR/.tmux.conf" "$HOME/.tmux.conf"
+ln -s "$DIR/tpm" "$HOME/.tmux/plugins/tpm"
+
