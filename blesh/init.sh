182e623fcf33a5e204e3946fdb7b7b0ba9b0091e
Dan Cassidy <5643061+rux616@users.noreply.github.com>
2020-05-11 04:41:02 +0000

    Add ble.sh

---
+++ blesh/init.sh
@@ -0,0 +1,5 @@
+# https://github.com/akinomyoga/ble.sh/wiki/Manual-%C2%A74-Editing#41213-bleopt-history_share-emptynon-empty-v04
+bleopt history_share=1
+
+# https://github.com/akinomyoga/ble.sh/wiki/Manual-%C2%A77-Completion#722-bleopt-complete_menu_style-v03
+bleopt complete_menu_style=align
