cb89a50c163b71dc304ce35a2f0add5b68443313
Dan Cassidy <5643061+rux616@users.noreply.github.com>
2020-05-12 09:02:44 +0000

    Set up ble.sh

--- blesh/init.sh
+++ blesh/init.sh
@@ -1,5 +1,33 @@
+# ble.sh manual: https://github.com/akinomyoga/ble.sh/wiki
+
+
+# immediately save history to $HISTFILE and share history between terminals
 # https://github.com/akinomyoga/ble.sh/wiki/Manual-%C2%A74-Editing#41213-bleopt-history_share-emptynon-empty-v04
 bleopt history_share=1
 
-# https://github.com/akinomyoga/ble.sh/wiki/Manual-%C2%A77-Completion#722-bleopt-complete_menu_style-v03
-bleopt complete_menu_style=align
+function my/keymap-settings-for-emacs-mode {
+  # bind C-h to backwards delete word
+  # https://github.com/akinomyoga/ble.sh/wiki/Manual-%C2%A73-Key-Binding#33-keymap-keymap
+  # https://github.com/akinomyoga/ble.sh/wiki/Manual-%C2%A74-Editing#44-wordwise-movement-and-operation
+  ble-bind -m emacs -f 'C-h' 'delete-backward-cword'
+}
+
+# create a function to be called through hook complete_load
+# https://github.com/akinomyoga/ble.sh/wiki/Manual-%C2%A77-Completion#7-completion
+function my/complete-load-hook {
+  # have auto-complete menu be only a single space between entries
+  # https://github.com/akinomyoga/ble.sh/wiki/Manual-%C2%A77-Completion#722-bleopt-complete_menu_style-v03
+  bleopt complete_menu_style=dense-nowrap
+
+  # ignore case for completion
+  # https://github.com/akinomyoga/ble.sh/wiki/Manual-%C2%A77-Completion#719-readline-variable-completion-ignore-case
+  bind 'set completion-ignore-case on'
+
+  # add siffixes to filename completions
+  # https://github.com/akinomyoga/ble.sh/wiki/Manual-%C2%A77-Completion#7111-readline-variable-visible-stats
+  bind 'set visible-stats on'
+}
+
+# load hooks
+blehook/eval-after-load keymap_emacs_load my/keymap-settings-for-emacs-mode
+blehook/eval-after-load complete my/complete-load-hook
